5d5138c90ab218fc283ff50c507df4dab849c6b3
Deya Hernández <thebestislost@gmail.com>
2026-04-17 20:37:56 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,149 +1,71 @@
 use anchor_lang::prelude::*;
 
-declare_id!("HyGro9GDxJ553WNFQ9yuvcNgzwvxMXZ1jyBhc6bsv8wP");
+declare_id!("");
 
 #[program]
+
 pub mod biblioteca {
-    use super::*;
+    pub fn crear_biblioteca() -> Result<()> {
 
-    pub fn crear_biblioteca(context: Context<NuevaBiblioteca>, nombre: String) -> Result<()> {
-        let owner = context.accounts.owner.key();
-        let libros: Vec<Libro> = Vec::new();
-
-        context.accounts.biblioteca.set_inner(Biblioteca {
-            owner,
-            nombre,
-            libros,
-        });
-
-        msg!("Biblioteca creada exitosamente!");
-        Ok(())
     }
 
-    pub fn agregar_libro(context: Context<NuevoLibro>, nombre: String, paginas: u16) -> Result<()> {
-        require!(
-            context.accounts.biblioteca.owner == context.accounts.owner.key(),
-            Errores::NoEresElOwner
-        );
+    pub fn agregar_libro() -> Result<()> {
 
-        let libro = Libro {
-            nombre,
-            paginas,
-            disponible: true,
-        };
-
-        context.accounts.biblioteca.libros.push(libro);
-        Ok(())
-    }
-
-    pub fn ver_libros(context: Context<NuevoLibro>) -> Result<()> {
-        require!(
-            context.accounts.biblioteca.owner == context.accounts.owner.key(),
-            Errores::NoEresElOwner
-        );
-
-        msg!(
-            "La lista de libros actualmente es: {:#?}",
-            context.accounts.biblioteca.libros
-        );
-
-        Ok(())
-    }
-
-    pub fn eliminar_libro(context:Context<NuevoLibro>, nombre: String) -> Result<()> {
-        require!(
-            context.accounts.biblioteca.owner == context.accounts.owner.key(),
-            Errores::NoEresElOwner
-        );
-
-        let libros = &mut context.accounts.biblioteca.libros;
-
-        for libro in 0..libros.len() {
-            if libros[libro].nombre == nombre {
-                libros.remove(libro);
-                msg!("Libro {} eliminado!", nombre);
-                return Ok(());
-            }
-        }
-
-        Err(Errores::LibroNoExiste.into())
-    }
-
-    pub fn alternar_estado(context:Context<NuevoLibro>, nombre: String) -> Result<()> {
-        require!(
-            context.accounts.biblioteca.owner == context.accounts.owner.key(),
-            Errores::NoEresElOwner
-        );
-
-        let libros = &mut context.accounts.biblioteca.libros;
-
-        for libro in 0..libros.len() {
-            let estado = libros[libro].disponible;
-
-            if libros[libro].nombre == nombre {
-                let nuevo_estado = !estado;
-                libros[libro].disponible = nuevo_estado;
-                msg!("El libro {} ahora esta {}", nombre, nuevo_estado);
-                return Ok(());
-            }
-        }
-
-        Err(Errores::LibroNoExiste.into())
     }
 }
 
-#[error_code]
-pub enum Errores {
-    #[msg("Error: No eres el propietario de la biblioteca que deseas modificar.")]
-    NoEresElOwner,
-
-    #[msg("Error: El libro proporcionado no existe.")]
-    LibroNoExiste,
-}
-
-#[account]
 #[derive(InitSpace)]
-pub struct Biblioteca {
+
+pub struct Biblioteca{
     owner: Pubkey,
+
+
 
     #[max_len(60)]
     nombre: String,
 
-    #[max_len(10)]
+     #[max_len(10)]
     libros: Vec<Libro>,
+
 }
 
 #[derive(InitSpace, AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Debug)]
-pub struct Libro {
+pub struct  Libro{
+
     #[max_len(60)]
     nombre: String,
 
     paginas: u16,
 
     disponible: bool,
+
 }
 
 #[derive(Accounts)]
-pub struct NuevaBiblioteca<'info> {
-    #[account(mut)]
-    pub owner: Signer<'info>,
+pub struct NuevaBiblioteca{
 
-    #[account(
+    pub owner: Singer<'info>,
+
+    #[account](
         init,
         payer = owner,
-        space = Biblioteca::INIT_SPACE + 8,
-        seeds = [b"biblioteca", owner.key().as_ref()],
-        bump
-    )]
+        space = Biblioteca::INIT_SPACE + 8, 
+        seeds = [b"biblioteca", owner.key().as_ref()]
+    )
+
     pub biblioteca: Account<'info, Biblioteca>,
 
     pub system_program: Program<'info, System>,
+
+
+
 }
 
 #[derive(Accounts)]
-pub struct NuevoLibro<'info> {
-    pub owner: Signer<'info>,
+pub struct NuevoLibro{
+        pub owner: Signer <'info>,
 
-    #[account(mut)]
-    pub biblioteca: Account<'info, Biblioteca>,
+        #[account(mut)]
+        pub biblioteca: Account<'info, Biblioteca>,
+
 }
